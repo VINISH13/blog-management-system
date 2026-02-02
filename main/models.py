8afc456871d1c3ef247399f562cae75f7b4d0d7e
VINISH A <vinisha@VINISHs-MacBook-Air.local>
2026-02-02 10:41:18 +0530

    Updated project files

--- main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class UserData(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    status = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
